8e9452a24cd0d0b8abbd5ebe0d0cfdbf97dfdbb0
Katerina Bolshakova <e.bolshakova2010@gmail.com>
2023-08-17 20:49:34 +0300

    feat: add module for page logout, implemented login and password on the login page

---
+++ src/app/modules/pageLogin/PageLogout.ts
@@ -0,0 +1,32 @@
+import Component from '../../components/Component';
+import LogoutForm from '../../components/LoginForm/LogoutForm/LogoutForm';
+import navItems from '../../data/navItems';
+import changePage from './helpers/ChangePage';
+
+export default class PageLogout extends Component {
+  render = () => {
+    this.content = new LogoutForm().render();
+    this.content.addEventListener('click', (e) => {
+      const el = e.target as HTMLElement;
+      if (el.classList.contains('yesBtn')) {
+        navItems.map((x) => {
+          const a = x;
+          if (a.href === 'Profile') {
+            a.title = 'Profile';
+          }
+          if (a.title === 'Logout') {
+            a.title = 'Login';
+            a.href = '/Login/';
+          }
+          return a;
+        });
+        changePage('/login/');
+      }
+      if (el.classList.contains('noBtn')) {
+        e.preventDefault();
+        changePage('/');
+      }
+    });
+    return this.content;
+  };
+}
---
+++ src/app/modules/pageLogin/helpers/ChangePage.ts
@@ -0,0 +1,6 @@
+const changePage = (namePage: string): void => {
+  const url = `${window.location.origin}${namePage}`;
+  window.history.pushState(null, '', url);
+  window.dispatchEvent(new Event('popstate'));
+};
+export default changePage;
