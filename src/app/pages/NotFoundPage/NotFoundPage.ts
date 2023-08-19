78aea101cd261746a00f72f419fbdbb82938b757
Katerina Bolshakova <e.bolshakova2010@gmail.com>
2023-08-19 17:52:18 +0300

    feat: added NodFound Page

--- src/app/pages/NotFoundPage/NotFoundPage.ts
+++ src/app/pages/NotFoundPage/NotFoundPage.ts
@@ -1,9 +1,10 @@
 import PageHeader from '../../modules/pageHeader/PageHeader';
+import PageNotFound from '../../modules/pageNotFound/PageNotFound';
 import Page from '../Page';
 
 export default class NotFoundPage extends Page {
   create() {
     this.page.appendChild(new PageHeader().render());
-    this.page.innerHTML += '404 Page';
+    this.page.appendChild(new PageNotFound().render());
   }
 }
