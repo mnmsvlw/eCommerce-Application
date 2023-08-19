9c20479af7dae1bb7542f6851e59109ae60e1b14
Katerina Bolshakova <e.bolshakova2010@gmail.com>
2023-08-19 15:45:16 +0300

    feat: added responsive application design for Logout page

--- src/app/components/LoginForm/LogoutForm/LogoutForm.ts
+++ src/app/components/LoginForm/LogoutForm/LogoutForm.ts
@@ -7,13 +7,15 @@ import './LogoutForm.css';
 export default class LogoutForm extends Component {
   render = () => {
     this.content = new Container('wrapperLogaut').render();
+    const formContainer = new Container('logoutForm-container').render();
     const text = 'Are you sure you want logout?';
-    const formHeader = new Heading(2, 'logoutForm-header', text).render();
+    const formHeader = new Heading(3, 'logoutForm-header', text).render();
     const box = new Container('btn-container').render();
     const noBtn = new Link('/', 'noBtn', 'NO').render();
     const yesBtn = new Link('/login/', 'yesBtn', 'YES').render();
     box.append(noBtn, yesBtn);
-    this.content.append(formHeader, box);
+    formContainer.append(formHeader, box);
+    this.content.append(formContainer);
     return this.content;
   };
 }
