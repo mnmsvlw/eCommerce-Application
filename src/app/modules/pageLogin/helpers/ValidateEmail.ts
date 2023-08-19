68dc95a68e72d4cd4103b5fe69b13a5941c0a4bd
Katerina Bolshakova <e.bolshakova2010@gmail.com>
2023-08-19 13:56:02 +0300

    fix: added checking mail for the possibility to contain 1 letter in the name

--- src/app/modules/pageLogin/helpers/ValidateEmail.ts
+++ src/app/modules/pageLogin/helpers/ValidateEmail.ts
@@ -2,7 +2,7 @@ export default function validateEmail(email: string): string {
   const re =
     /^(?:[a-z0-9!#$%&amp;'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&amp;'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/;
   const valid = re.test(email);
-  const ename = /^[0-9a-z]{1}[-_0-9a-z.]{0,30}[0-9a-z]{1}$/i.test(email.toLowerCase().split('@')[0]);
+  const ename = /^[0-9a-z]{1}[-_0-9a-z.]{0,30}[0-9a-z]{0,30}$/i.test(email.toLowerCase().split('@')[0]);
   const edog = /^([-_0-9a-z.]+@)/.test(email.toLowerCase());
   const edog1 = [...email].filter((x) => x === '@').length === 1;
   const domain = email.split('@')[1];
