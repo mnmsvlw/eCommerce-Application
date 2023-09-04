7a7804734cdf40f68b5015399d8d5f3017b544fc
Vika Lubenets <lubenetcvika@gmail.com>
2023-09-04 22:13:50 +0400

    fix: resolve conflict

--- src/app/pages/ProductPage/ProductPage.ts
+++ src/app/pages/ProductPage/ProductPage.ts
@@ -6,7 +6,7 @@ import Page from '../Page';
 export default class RegisterPage extends Page {
   async create() {
     this.page.appendChild(new Header().render());
-    this.page.appendChild(new ProductModule().render('1'));
+    this.page.appendChild(new ProductModule().render(''));
     listenBurger(this.page);
   }
 }
