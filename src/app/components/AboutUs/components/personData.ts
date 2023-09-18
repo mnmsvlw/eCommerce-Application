dc9f52404f0360541b97822e0f79fa6884b7157c
Katerina Bolshakova <e.bolshakova2010@gmail.com>
2023-09-18 15:15:43 +0300

    feat: added data collaboration and style

--- src/app/components/AboutUs/components/personData.ts
+++ src/app/components/AboutUs/components/personData.ts
@@ -5,11 +5,10 @@ const Katerina: Person = {
   role: 'Web-developer',
   url: './assets/Katerina.jpg',
   biography:
-    'Bio: a 34-year-old mom decided to explore a new path during a maternity leave. Left behind a 10-year career in sales to pursue a passion for web development',
+    'Bio: a 34-year-old mom decided to explore a new path during a maternity leave. Left behind a 10-year career in sales to pursue a passion for web development.',
   gitLink: 'https://github.com/Katerina2010',
-  inLink: 'https://ru.linkedin.com/',
-  contribution:
-    'Some text here ... Some text here ... Some text here ...Some text here ...Some text here ...Some text here ...Some text here ...Some text here ...Some',
+  inLink: 'https://www.linkedin.com/in/kat-cv/',
+  contribution: `Contributed significantly to the project by  implementing key components such as the login page with form validation, the shopping cart page, the user profile page, and the 'About Us' page. `,
 };
 
 const Viktoriya: Person = {
@@ -32,8 +31,7 @@ const Vladimir: Person = {
     'Bio: text text text ..... text text text ..... text text text ..... text text text ..... text text text ..... text text text ..... text text text ..... text text text ..... text text text ..... text text text ..... text text text ..... text text text .....',
   gitLink: 'https://github.com/mnmsvlw',
   inLink: 'https://ru.linkedin.com/',
-  contribution:
-    'Some text here ... Some text here ... Some text here ... Some text here ...Some text here ...Some text here ...Some text here ...Some text here ...Some text here ...Some',
+  contribution: `Played a crucial role as the team lead, overseeing the project's overall development. Responsibilities included setting up and managing API interactions, writing website routing, designing the project's architecture and basic classes, composing a substantial portion of the project's tests, and creating the catalog page along with all product filters.`,
 };
 
 export { Katerina, Vladimir, Viktoriya };
