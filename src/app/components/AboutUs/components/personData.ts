483ed301230d40eb47ae70fcfbe19d0d35f638aa
Katerina Bolshakova <e.bolshakova2010@gmail.com>
2023-09-18 23:00:26 +0300

    refactor: extra lines and commented code removed

--- src/app/components/AboutUs/components/personData.ts
+++ src/app/components/AboutUs/components/personData.ts
@@ -3,7 +3,6 @@ import { Person } from '../../../../types/personTypes';
 const Katerina: Person = {
   name: 'Katerina',
   role: 'Web-developer',
-  url: './assets/Katerina.jpg',
   biography:
     'Bio: a 34-year-old mom decided to explore a new path during a maternity leave. Left behind a 10-year career in sales to pursue a passion for web development.',
   gitLink: 'https://github.com/Katerina2010',
@@ -14,7 +13,6 @@ const Katerina: Person = {
 const Viktoriya: Person = {
   name: 'Viktoriya',
   role: 'Web-developer',
-  url: './fotoViktoriya',
   biography:
     'Bio: a 25-year-old former international lawyer who transitioned to web development after gaining 5 years of experience in legal support for IT, startups, and M&A projects.',
   gitLink: 'https://github.com/VikaLubenets',
@@ -26,7 +24,6 @@ const Viktoriya: Person = {
 const Vladimir: Person = {
   name: 'Vladimir',
   role: 'Team Lead',
-  url: './fotoVladimir',
   biography:
     'Bio: a 25-years-old medical professional with a programming background aspiring to become a programmer with a medical background.',
   gitLink: 'https://github.com/mnmsvlw',
--- src/types/personTypes.ts
+++ src/types/personTypes.ts
@@ -1,7 +1,6 @@
 export interface Person {
   name: string;
   role: string;
-  url: string;
   biography: string;
   gitLink: string;
   inLink: string;
